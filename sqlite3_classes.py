3615bee1936e1e5507e3fdf1ebd6a3ee14781de4
Shahab Najmosadaty <shahabnajmosadaty@outlook.com>
2025-02-25 13:10:01 +0330

    Searching def has been completed

--- sqlite3_classes.py
+++ sqlite3_classes.py
@@ -1,14 +1,45 @@
+""" SQLite3 Custom
+This file allows the program to create, modify, delete and etc on Database
+Based on sqlite3 with using import sqlite3
+"""
+
 import sqlite3
 import os
+import glob
 
 
 def check_if_database_exists():
-    name = "*.db"
-    path = os.getcwd()
-    print(path)
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
+    """
+    # name = "shahab.db"
+    # path = os.getcwd()
+    # print(path)
+    # results = []
+    # for root, dirs, files in os.walk(path):
+    #     if name in files:
+    #         results.append(os.path.join(root, name))
+    # return results
+
+    Cause of not working os.walk(path)
+    I had to use glob
+    Documention link is below :
+    https://www.geeksforgeeks.org/file-searching-using-python/
+
+    """
+
+    """A list for files"""
+    results = []
+
+    """Define files"""
+
+    files = glob.glob("*.db")
+
+    """Searching in files whether a file extension is .db"""
+
+    for file in files:
+        results.append(file)
+
+    """Calling back the results of search"""
+    return results
 
 
 print(check_if_database_exists())
